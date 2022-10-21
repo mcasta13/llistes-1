cdd752b83f3fd6a30097853eb5e60396e4233add
mcasta13 <116083045+mcasta13@users.noreply.github.com>
2022-10-21 20:03:06 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    encarregat = 0
+})
+let encarregat = 0
+let A1 = 0
+let A2 = 0
+let A3 = 0
+let A4 = 0
+let disponibles = [
+A1,
+A2,
+A3,
+A4
+]
+let ocupats: number[] = []
+basic.forever(function () {
+	
+})
